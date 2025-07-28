b19391c364e5b31f7923da52c7db1117607d6baf
rossfreeman23 <rossfreeman23@gmail.com>
2025-07-27 23:58:24 -0600

    Update noun.py

--- models/noun.py
+++ models/noun.py
@@ -1,1 +1,7 @@
-
+from models.enums import Case, Number, Gender, Mutation
+class Noun:
+    def __init__(self, case, number, gender, mutation):
+        self.case = case
+        self.number = number
+        self.gender = gender
+        self.mutation = mutation
