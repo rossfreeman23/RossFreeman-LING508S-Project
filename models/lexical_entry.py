c5fdd154b4be203e4db4569bcc71b42c8db92caa
rossfreeman23 <rossfreeman23@gmail.com>
2025-07-27 23:57:48 -0600

    Update lexical_entry.py

--- models/lexical_entry.py
+++ models/lexical_entry.py
@@ -1,1 +1,7 @@
-
+from models.enums import PartOfSpeech, Mutation
+class LexicalEntry:
+    def __init__(self, form, part_of_speech, definition, mutation):
+        self.form = form
+        self.part_of_speech = part_of_speech
+        self.definition = definition
+        self.mutation = mutation
