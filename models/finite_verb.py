b5187ba190ab456c670f38155c9af464af81c2f1
rossfreeman23 <rossfreeman23@gmail.com>
2025-07-27 23:58:50 -0600

    Update finite_verb.py

--- models/finite_verb.py
+++ models/finite_verb.py
@@ -1,1 +1,8 @@
-
+from models.enums import Person, Number, Tense, Voice, Mood
+class FiniteVerb:
+    def __init__(self, person, number, tense, voice, mood):
+        self.person = person
+        self.number = number
+        self.tense = tense
+        self.voice = voice
+        self.mood = mood
