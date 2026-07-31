0bc9f1bc0441f3782562758857d10676fc5c11b4
rossfreeman23 <rossfreeman23@gmail.com>
2026-07-30 18:04:22 -0700

    Update models.py
    addresses feedback on classes reacting to and not accepting nouns not found in db

--- models/models.py
+++ models/models.py
@@ -28,6 +28,7 @@ class PartOfSpeech(Enum):
     ADJECTIVE = 3
 
 #NounForm_Class
+#represents one inflected form of noun
 class NounForm:
     def __init__(
         self,
@@ -36,12 +37,29 @@ class NounForm:
         number: Number,
         mutation: Mutation
     ):
+        if not isinstance(surface_form, str):
+            raise TypeError(
+                "surface_form must be a string"
+            )
+        if not isinstance(case, Case):
+            raise TypeError(
+                "case must be a Case value"
+            )
+        if not isinstance(number, Number):
+            raise TypeError(
+                "number must be a Number value"
+            )
+        if not isinstance(mutation, Mutation):
+            raise TypeError(
+                "mutation must be a Mutation value"
+            )
         self.surface_form = surface_form
         self.case = case
         self.number = number
         self.mutation = mutation
 
 #Noun_Class
+#stores noun-specific grammatical information that affects NounFomr i.e. inflected form
 class Noun:
     def __init__(
         self,
@@ -49,11 +67,24 @@ class Noun:
         declension: Declension,
         forms: list[NounForm]
     ):
+        if not isinstance(gender, Gender):
+            raise TypeError(
+                "gender must be a Gender value"
+            )
+        if not isinstance(declension, Declension):
+            raise TypeError(
+                "declension must be a Declension value"
+            )
+        if not all(isinstance(form, NounForm) for form in forms):
+            raise TypeError(
+                "forms must contain only NounForm objects"
+            )
         self.gender = gender
         self.declension = declension
         self.forms = forms
 
 #LexicalEntry_Class
+#represents dictionary entry of given word essentially
 class LexicalEntry:
     def __init__(
         self,
@@ -62,17 +93,42 @@ class LexicalEntry:
         part_of_speech: PartOfSpeech,
         noun_data: Noun = None
     ):
+        if not isinstance(lemma, str):
+            raise TypeError(
+                "lemma must be a string"
+            )
+        if not isinstance(definition, str):
+            raise TypeError(
+                "definition must be a string"
+            )
+        if not isinstance(part_of_speech, PartOfSpeech):
+            raise TypeError(
+                "part_of_speech must be a PartOfSpeech value"
+            )
+        if noun_data is not None and not isinstance(noun_data, Noun):
+            raise TypeError(
+                "noun_data must be a Noun object"
+            )
         self.lemma = lemma
         self.definition = definition
         self.part_of_speech = part_of_speech
         self.noun_data = noun_data
 
 #Word_Class
+#represents user input
 class Word:
     def __init__(
         self,
         surface_form: str,
         entries: list[LexicalEntry]
     ):
+        if not isinstance(surface_form, str):
+            raise TypeError(
+                "surface_form must be a string"
+            )
+        if not all(isinstance(entry, LexicalEntry) for entry in entries):
+            raise TypeError(
+                "entries must contain only LexicalEntry objects"
+            )
         self.surface_form = surface_form
         self.entries = entries
