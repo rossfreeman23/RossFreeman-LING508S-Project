285c3e70fe027ce13a906531b842995f63e4f2a4
rossfreeman23 <rossfreeman23@gmail.com>
2026-07-31 13:44:39 -0700

    Create config.py
    as previously stated, allows mysqlrepository & parser service to function

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py: provides MySQL conenction settings used by repository layer
+# placed alongside app.py in project root
+import os
+config = {
+    "host": os.getenv("MYSQL_HOST", "localhost"),
+    "user": os.getenv("MYSQL_USER", "root"),
+    "password": os.getenv("MYSQL_PASSWORD", ""),
+    "database": os.getenv("MYSQL_DATABASE", "grammar_db"),
+    "charset": "utf8mb4"
+}
